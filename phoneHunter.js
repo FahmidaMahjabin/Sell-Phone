9ee4fc884ca1bfc940943796b2d76fd626319bfb
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2022-03-01 13:03:11 +0600

    add modal for each phone

--- phoneHunter.js
+++ phoneHunter.js
@@ -41,8 +41,30 @@ const showEachPhone = (phone) =>{
     <div class="card-body">
       <h5 class="card-title">${phone.phone_name}</h5>
       <h5 class="card-text">${phone.brand}</h5>
-      <a href="https://openapi.programming-hero.com/api/phone/${phone.slug}" target = "_blank" class="btn btn-primary">See Details</a>
-    </div></div>`;
+      <a  onclick = 'showPhoneDetail("${phone.slug}")'  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >See Details</a>
+    </div>
+    </div>`;
     document.getElementById("phones").appendChild(aPhone);
+}
+// function = show one phone detailes when click 
+// step1:when click on the phone details :
+//     step1.1:get the phone.slug property and add that to the url 
+//     step1.2:fetch url and get data 
+//     step1.3:get phone image and all data into toe body and show in a div
+
+const showPhoneDetail = (phoneModel) => {
+    
+    console.log(phoneModel);
+    fetch(`https://openapi.programming-hero.com/api/phone/${phoneModel}`)
+    .then(response => response.json())
+    .then(data => showEachDetails(data.data))
+
+}
+// showPhoneDetail()
+// step1:ekta card add korbo jate phone er shob detaile add korbo 
+// step2: card ta browser e innerHTML e add korbo
+const showEachDetails = (phone) => {
+        
+
 }
 
